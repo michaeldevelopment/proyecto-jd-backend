d652e8632e0a8c905b52405697cc6238efcdd421
Michael Sanabria <maicolsana12@gmail.com>
2024-11-18 15:20:57 -0500

    nuevas correcciones

--- index.js
+++ index.js
@@ -2,23 +2,71 @@ const bodyParser = require("body-parser");
 const express = require("express");
 const cors = require("cors");
 const predict = require("./train");
+const splitIntoChunks = require("./utils/splitIntoChunks");
+const { default: axios } = require("axios");
 
 const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "*",
+    credentials: true,
+    optionSuccessStatus: 200,
   })
 );
 
 app.use(express.json());
 app.use(bodyParser.json());
 
+const isBetween = (x, min, max) => {
+  return x >= min && x <= max;
+};
+
 app.post("/predict", async (req, res) => {
   try {
-    const result = await predict(req.body);
-    console.log("result: ", result);
-    return res.status(202).send({ status: "done", result });
+    const {
+      data: { data },
+    } = await axios.get(
+      `https://api.weatherbit.io/v2.0/forecast/hourly?lat=7.8891&lon=-72.4967&key=a4397c2ccdf84bda84e4b79e50d5ef83&hours=${req.body.horas}`
+    );
+
+    const primerDia = String(data[0].timestamp_local);
+    const tempActual = data[0].app_temp;
+
+    const apiData = data.map((data) => {
+      return {
+        hora: new Date(data.timestamp_local).getHours(),
+        temperatura: data.app_temp,
+        precipitacion: data.precip,
+      };
+    });
+
+    const resultPrediction = await predict(apiData);
+    const result = splitIntoChunks(
+      apiData.map((element, index) => {
+        return {
+          ...element,
+          irradiancia:
+            isBetween(element.hora, 0, 5) || isBetween(element.hora, 19, 23)
+              ? 0
+              : resultPrediction[index] < 0
+              ? 0
+              : resultPrediction[index],
+        };
+      })
+    );
+
+    result.shift();
+    result.pop();
+
+    console.log("resultado final estimacion => ", result.flat());
+
+    return res.status(202).send({
+      status: "done",
+      result: result.flat(),
+      primerDia,
+      tempActual,
+    });
   } catch (e) {
     console.log(e);
     return res.status(500).json({ status: "error", message: e });
